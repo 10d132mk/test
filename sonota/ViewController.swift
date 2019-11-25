c2261b1743e0c44e54cb01653d9157afe9054997
10d132mk <10d132mk@gmail.com>
2019-11-25 19:28:13 +0900

    Initial Commit

---
+++ sonota/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sonota
+//
+//  Created by 黒川ももこ on 2019/11/25.
+//  Copyright © 2019 10d132mk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
